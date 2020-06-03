5f625443ad672faae9931b15371e2691bd9cdf1f
sinistergeek <susanbhattraidon@gmail.com>
2020-06-03 12:24:20 +0000

    Add files via upload

---
+++ file_CV.py
@@ -0,0 +1,36 @@
+print('''
+
+    +++++++++++++++++++++
+    |                   |
+    |    PDF            |
+    |       To          |
+    |         MP3       |
+    |     Converter     |
+    |                   |
+    +++++++++++++++++++++
+
+
+''')
+
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+import pdftotext
+from gtts import gTTS
+import sys
+
+Tk().withdraw()
+filelocation = askopenfilename()
+
+
+with open(filelocation, "rb") as red:
+    pdf = pdftotext.PDF(red)
+
+string_of_text=""
+for text in pdf:
+    string_of_text += text
+
+final_file = gTTS(text=string_of_text, lang='en')
+
+uname_file = input("\n\nPlease ! Enter the File Name: ")
+final_file.save(uname_file)
+ Print("Thank You" + uname_file + "has been converted!!")
